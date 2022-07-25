4b16b0da00476fe582e083195c4cd77128d47a5c
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-07-26 00:07:54 +0200

    initialise

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/missions" />
+          <Route exact path="/rockets" />
+          <Route
+            path="*"
+            element={
+              <section className="flex-center"><div className="container text-center">404 Page not found</div></section>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
